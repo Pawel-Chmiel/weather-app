b1de2653f9de3effa2bac1dfd7fcf6281341b5cf
Paweł Chmiel <pawelchmiel7@gmail.com>
2020-05-11 13:58:10 +0200

    Updates

---
+++ precache-manifest.377c11c4932c9ae4e2592733d7610227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d130e83cc663296fcfc9a5825de0f11e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "faf74ed25771b77b0c74",
+    "url": "/weather-app/static/css/main.6c5c65e6.chunk.css"
+  },
+  {
+    "revision": "8c8ebc072ccfe9dd3873",
+    "url": "/weather-app/static/js/2.d3c38379.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather-app/static/js/2.d3c38379.chunk.js.LICENSE"
+  },
+  {
+    "revision": "faf74ed25771b77b0c74",
+    "url": "/weather-app/static/js/main.3342296e.chunk.js"
+  },
+  {
+    "revision": "48f3787e04d45de6cbd1",
+    "url": "/weather-app/static/js/runtime-main.36d91d67.js"
+  }
+]);
